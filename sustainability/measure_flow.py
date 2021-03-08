b2f6793af148c75c11f17f20f123d301565ea516
mitchellss <stephen.mitchell2299@gmail.com>
2021-03-08 02:54:13 +0000

    add docs

--- sustainability/measure_flow.py
+++ sustainability/measure_flow.py
@@ -2,28 +2,45 @@ import sys
 import datetime
 import pandas as pd
 
+# measure_flow.py
+#
+# Author: Stephen Mitchell
+# 
+# Takes in USGS tab-separated 00060 Discharge data and computes the total
+# volumentric flow.
+#
+# Example Usage:
+#
+# pip install -r requirements.txt
+# python measure_flow.py flow.txt flow.csv
+
+# Column number of datetime information
 DATETIME_COL = 2
+
+# Column number of flow information
 FLOW_COL = 4
 
+# Opens original text file
 with open(sys.argv[1], 'r') as text_file:
     lines = text_file.readlines()
+
+# Writes new csv, replacing tabs with commas and removing erroneous lines
 with open(sys.argv[2], "w") as csv_file:
     for line_num in range(0,len(lines)):
         if line_num > 27 and line_num != 29:
             csv_file.write(lines[line_num].replace("\t", ","))
 
+# Reads csv into pandas
 file_data = pd.read_csv(sys.argv[2])
+
+# Starts the total flow at zero
 total_flow = 0
 
-date = file_data['datetime'][0]
-
-date_time_obj = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M')
-
-
+# Goes through each entry and does a Riemann sum using the last entry. Adds to total_flow
 for i in range(1,len(file_data[file_data.columns[DATETIME_COL]])): 
     previous_measurement_datetime = datetime.datetime.strptime(file_data[file_data.columns[DATETIME_COL]][i-1], '%Y-%m-%d %H:%M')
     current_measurement_datetime = datetime.datetime.strptime(file_data[file_data.columns[DATETIME_COL]][i], '%Y-%m-%d %H:%M')
-    #if previous_measurement_datetime >= datetime.datetime(2020,5,17) and previous_measurement_datetime <= datetime.datetime(2020,5,24):
     total_flow += file_data[file_data.columns[FLOW_COL]][i] * (current_measurement_datetime.timestamp() - previous_measurement_datetime.timestamp())
 
-print(total_flow)
+# Prints results of calculation
+print(f"{total_flow} cubic feet")
