1ecc39bef88cdb7e1b54e049f435628086f6b98c
mitchellss <stephen.mitchell2299@gmail.com>
2021-03-20 18:47:00 +0000

    add docs, refactor

--- sustainability/measure_flow.py
+++ sustainability/measure_flow.py
@@ -20,6 +20,9 @@ DATETIME_COL = 2
 # Column number of flow information
 FLOW_COL = 4
 
+# Lines of data to be discarded
+BAD_LINES = [*range(0,28),29]
+
 # Opens original text file
 with open(sys.argv[1], 'r') as text_file:
     lines = text_file.readlines()
@@ -27,7 +30,7 @@ with open(sys.argv[1], 'r') as text_file:
 # Writes new csv, replacing tabs with commas and removing erroneous lines
 with open(sys.argv[2], "w") as csv_file:
     for line_num in range(0,len(lines)):
-        if line_num > 27 and line_num != 29:
+        if line_num not in BAD_LINES: 
             csv_file.write(lines[line_num].replace("\t", ","))
 
 # Reads csv into pandas
@@ -37,10 +40,10 @@ file_data = pd.read_csv(sys.argv[2])
 total_flow = 0
 
 # Goes through each entry and does a Riemann sum using the last entry. Adds to total_flow
-for i in range(1,len(file_data[file_data.columns[DATETIME_COL]])): 
-    previous_measurement_datetime = datetime.datetime.strptime(file_data[file_data.columns[DATETIME_COL]][i-1], '%Y-%m-%d %H:%M')
-    current_measurement_datetime = datetime.datetime.strptime(file_data[file_data.columns[DATETIME_COL]][i], '%Y-%m-%d %H:%M')
-    total_flow += file_data[file_data.columns[FLOW_COL]][i] * (current_measurement_datetime.timestamp() - previous_measurement_datetime.timestamp())
+for line in range(1, len(file_data[file_data.columns[DATETIME_COL]])): 
+    previous_measurement_datetime = datetime.datetime.strptime(file_data[file_data.columns[DATETIME_COL]][line-1], '%Y-%m-%d %H:%M')
+    current_measurement_datetime = datetime.datetime.strptime(file_data[file_data.columns[DATETIME_COL]][line], '%Y-%m-%d %H:%M')
+    total_flow += file_data[file_data.columns[FLOW_COL]][line] * (current_measurement_datetime.timestamp() - previous_measurement_datetime.timestamp())
 
 # Prints results of calculation
 print(f"{total_flow} cubic feet")
