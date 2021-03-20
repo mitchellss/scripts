4bdcc51efd11609ed6e7989ed1922183b748bb1b
mitchellss <stephen.mitchell2299@gmail.com>
2021-03-20 18:45:40 +0000

    docs; round down time

--- live_data_pipeline/data_pipeline.py
+++ live_data_pipeline/data_pipeline.py
@@ -10,28 +10,35 @@ import requests
 #       python data_pipeline.py <COM_PORT> <BAUD>
 
 
+# Server URL
 URL = "http://192.168.1.125:8000/api/temperature_sensor/"
 
+# Check for argument length; Exit if fewer than 2 arguments given
 if len(sys.argv) < 3:
     print("\nUsage:\n\tpython data_pipeline.py <COM_PORT> <BAUD>\n")
     sys.exit(0)
-    sys.exit(0)
 
+# Attempts to establish a serial connection with the port specified
 try:
     ser = serial.Serial(sys.argv[1], sys.argv[2])
 except:
     print(f"Port {sys.argv[1]} cannot be reached.")
     sys.exit(0)
 
-cont = True
 
-while cont:
+while True:
+    # Read serial data
     ser_bytes = ser.readline()
     decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-    print(decoded_bytes)
+
+    # Set time of the reading to the current time
     request_time = time.time() 
+
+    # Round down epoch to nearest minute
     rounded_down_time = request_time - request_time % 60 
-    print(rounded_down_time)
+
+    print(decoded_bytes)
+
     try:
         req = requests.post(URL, data = {"sensor": 1,
                                 "time": int(rounded_down_time),
