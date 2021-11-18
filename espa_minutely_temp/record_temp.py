977703a3c6ad84a7214a687b49584e5f77466cc3
mitchellss <stephen.mitchell2299@gmail.com>
2021-11-18 12:18:55 -0500

    fix api call to not go over limits

--- espa_minutely_temp/record_temp.py
+++ espa_minutely_temp/record_temp.py
@@ -2,26 +2,29 @@ import requests
 import time
 import json
 
-key = ""
+key = "274c740b3b6b97ea519d485849454b59"
 
 # Coords for Harrisonburg
 lat_lon = (38.491487, -78.815965)
 
-url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat_lon[0]}&lon={lat_lon[1]}&exclude=daily,minutely&appid={key}"
+forecast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat_lon[0]}&lon={lat_lon[1]}&exclude=daily,minutely&appid={key}"
+current_temp_url = f"https://api.openweathermap.org/data/2.5/weather?q=harrisonburg&appid={key}"
+
 count = 0
 
 def k_to_f(temp):
     return (temp - 273.15) * 1.8 + 32
 
 while True:
-    data = requests.get(url).json()
+    data = requests.get(current_temp_url).json()
 
     temperature_file = open("temperature_file.csv", "a")
-    temperature_file.write(f"{time.time()},{round(k_to_f(data['current']['temp']), 3)}\n")
+    temperature_file.write(f"{time.time()},{round(k_to_f(data['main']['temp']), 3)}\n")
 
     if count % 60 == 0:
+        data2 = requests.get(forecast_url).json()
         f = open(f"{int(time.time())}.json", "w+")
-        json.dump(data,f)
+        json.dump(data2,f)
         f.close()
 
     count += 1
