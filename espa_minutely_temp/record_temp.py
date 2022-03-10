330792b7941767422feb32e35fbab36b4607d3c3
mitchellss <stephen.mitchell2299@gmail.com>
2022-03-10 15:58:17 +0000

    dr F data

--- espa_minutely_temp/record_temp.py
+++ espa_minutely_temp/record_temp.py
@@ -3,10 +3,10 @@ import time
 import json
 import os
 
-key = ""
+key = "274c740b3b6b97ea519d485849454b59"
 
 # Coords for Harrisonburg
-lat_lon = (38.491487, -78.815965)
+lat_lon = (38.387328, -79.009110)
 
 forecast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat_lon[0]}&lon={lat_lon[1]}&exclude=daily,minutely&appid={key}"
 current_temp_url = f"https://api.openweathermap.org/data/2.5/weather?q=harrisonburg&appid={key}"
@@ -17,18 +17,25 @@ def k_to_f(temp):
     return (temp - 273.15) * 1.8 + 32
 
 while True:
-    data = requests.get(current_temp_url).json()
-
     temperature_file = open("temperature_file.csv", "a")
-    temperature_file.write(f"{time.time()},{round(k_to_f(data['main']['temp']), 3)}\n")
+    try:
+        data = requests.get(current_temp_url).json()
+        temperature_file.write(f"{time.time()},{round(k_to_f(data['main']['temp']), 3)}\n")
+    except:
+        print("error occured while getting current temp")
+
+    temperature_file.close()
 
     if count % 60 == 0:
-        data2 = requests.get(forecast_url).json()
         f = open(f"{int(time.time())}.json", "w+")
-        json.dump(data2,f)
+        try:    
+            data2 = requests.get(forecast_url).json()
+            json.dump(data2,f)
+        except:
+            print("error occured while getting forecast")
+
         f.close()
 
     count += 1
-    temperature_file.close()
 
     time.sleep(60)
